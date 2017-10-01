172b01123acb185b7fbf656ce1f90d72fe4dae5f
Kenneth Bolivar Castro <keboca@gmail.com>
2017-10-01 00:24:02 -0600

    Updated Logica - Datos - Added Pruebas

---
+++ cnsPruebas/UsuariosPruebas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using slnLogica;
+
+namespace cnsPruebas
+{
+    class UsuariosPruebas
+    {
+        public void ejecutar()
+        {
+            this.iniciarSession();
+        }
+
+        public void iniciarSession()
+        {
+            IServiciosUsuarios usuarios = new AccionesUsuarios();
+            if (usuarios.iniciarSession("keboca@gmail.com", "clave"))
+            {
+                Console.WriteLine("Session iniciada!");
+            }
+            else
+            {
+                Console.WriteLine("No se logra iniciar session.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ slnLogica/AccionesUsuarios.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using slnDatos;
+
+namespace slnLogica
+{
+    public interface IServiciosUsuarios
+    {
+        bool iniciarSession(string correo, string clave);
+    }
+
+    public class AccionesUsuarios : IServiciosUsuarios
+    {
+        private FacturacionEntidades contexto;
+        public AccionesUsuarios()
+        {
+            this.contexto = new FacturacionEntidades();
+        }
+
+        public bool iniciarSession(string correo, string clave)
+        {
+            // Encriptar clave.
+            Usuario usuario = this.contexto.Usuarios.FirstOrDefault(u => u.Email.Equals(correo));
+            return (usuario != null);
+        }
+    }
+}
