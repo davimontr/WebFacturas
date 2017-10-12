4c583a8e1dd584538abd27adb1b039bd471fbfa0
OlgerGarita <olgeragt@gmail.com>
2017-10-11 21:05:02 -0600

    metodos de usuarios finalizados y revisados correctamente

--- cnsPruebas/UsuariosPruebas.cs
+++ cnsPruebas/UsuariosPruebas.cs
@@ -7,36 +7,36 @@ using slnLogica;
 
 namespace cnsPruebas
 {
-    class UsuariosPruebas
+ class UsuariosPruebas
     {
         private IServiciosUsuarios usuarios = new AccionesUsuarios();
 
         public void ejecutar()
         {
-            this.iniciarSession();
+            //this.iniciarSession();
             //this.ModificarUsu();
-             //this.ingresarUsuario();
+            // this.ingresarUsuario();
             this.encriptarClave();
-            //this.eliminar();
+            this.eliminar();
             //this.encriptar();
         }
 
-        public void iniciarSession()
-        {
-            if (this.usuarios.iniciarSession("olgeragt@gmail.com", "12345"))
-            {
-                Console.WriteLine("Session iniciada!");
-            }
-            else
-            {
-                Console.WriteLine("No se logra iniciar session.");
-            }
-        }
+        //public void iniciarSession()
+        //{
+        //    if (this.usuarios.iniciarSession("hole@gmail.com", "141190"))
+        //    {
+        //        Console.WriteLine("Session iniciada!");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("No se logra iniciar session.");
+        //    }
+        //}
 
         //public void ModificarUsu()
         //{
 
-        //    this.usuarios.actualizaUsuario(1, "olgeragt@gmail.com", "12345,", 1);
+        //    this.usuarios.actualizaUsuario(1, "davidm@gmail.com", "777,", 1);
 
         //    Console.WriteLine("Usuario modificado!");
 
@@ -47,20 +47,20 @@ namespace cnsPruebas
 
         //public void ingresarUsuario()
         //{
-        //    this.usuarios.incluirUsuario("patito@gmail.com", "aaaa", 1);
+        //    this.usuarios.incluirUsuario("olgeragt@gmail.com", "777", 1);
         //    Console.WriteLine("Usuario agregado!");
         //}
 
-        //public void eliminar()
-        //{
-        // this.usuarios.eliminarUsuario(5);
-        //  Console.WriteLine("Usuario eliminado!");
-        //}
+        public void eliminar()
+        {
+            this.usuarios.eliminarUsuario(1);
+            Console.WriteLine("Usuario eliminado!");
+        }
 
         public void encriptarClave()
         {
             
-            Console.WriteLine("Encriptada: {0}", this.usuarios.encriptarClaveUsuario("12345"));
+            Console.WriteLine("Encriptada: {0}", this.usuarios.encriptarClaveUsuario("secreto"));
         }
 
     }
--- slnLogica/AccionesUsuarios.cs
+++ slnLogica/AccionesUsuarios.cs
@@ -12,9 +12,7 @@ namespace slnLogica
 
         List<Usuario> obtenerTodos();
         bool iniciarSession(string correo, string clave);
-        void actualizaClaveUsuario(int Id, string Clave);
         void actualizarCorreo(int Id, string correo);
-        bool existeCorreo(string Correo);
         string encriptarClaveUsuario(string clave);
         void incluirUsuario(string email, string contrasenna, int idrol);
         void eliminarUsuario(int id);
@@ -25,24 +23,26 @@ namespace slnLogica
     public class AccionesUsuarios : IServiciosUsuarios
     {
         private FacturacionEntidades contexto;
+
         public AccionesUsuarios()
         {
             this.contexto = new FacturacionEntidades();
         }
-        //inicio de session
+
+      //inicio de session
         public bool iniciarSession(string correo, string clave)
         {
-            // Encriptar clave.
+             
             string encriptada = this.encriptarClaveUsuario(clave);
-            Usuario usuario = this.contexto.Usuarios.FirstOrDefault(u => u.Email.Equals(correo )&&(u.Clave.Equals(clave)));
+            Usuario usuario = this.contexto.Usuarios.FirstOrDefault(u => u.Email.Equals(correo )&&(u.Clave.Equals(encriptada)));
             return (usuario != null);
 
         }
 
-
+        // Metodo de encriptar la clave
       public  string encriptarClaveUsuario(string clave)
         {
-  
+
             System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
             byte[] datos = md5.ComputeHash(Encoding.Default.GetBytes(clave));
             StringBuilder strConstructor = new StringBuilder();
@@ -56,29 +56,13 @@ namespace slnLogica
 
 
 
-        // metodo para actualizar la clave de usuario 
-        public void actualizaClaveUsuario(int Id, string Clave)
-        {
-
-            Usuario usuario = this.obtenUsuarioSegunIdentificador(Id);
-            string encriptada = this.encriptarClaveUsuario(Clave);
-            usuario.Clave = Clave;
-            this.encriptarClaveUsuario(Clave);
-            this.contexto.SaveChanges();
-        }
+        // metodo para actualizar el correo de usuario 
 
       public  void actualizarCorreo(int Id, string correo)
         {
-        
             Usuario usuario = this.obtenUsuarioSegunIdentificador(Id);
             usuario.Email = correo;
             this.contexto.SaveChanges();
-        }
-
-        // metodo  para verificar correo
-        public bool existeCorreo(string Correo)
-        {
-            return (this.contexto.Usuarios.Where(u => u.Email.Equals(Correo)).Count() == 1) ? true : false;
         }
 
 
@@ -93,8 +77,7 @@ namespace slnLogica
         public void incluirUsuario(string email, string contrasenna, int idrol)
         {
              string clave = this.encriptarClaveUsuario(contrasenna);
-            this.contexto.Usuarios.Add(new Usuario { Email = email, Clave = contrasenna, IdRol = idrol });
-            clave = this.encriptarClaveUsuario(contrasenna);
+            this.contexto.Usuarios.Add(new Usuario { Email = email, Clave = clave, IdRol = idrol });
             this.contexto.SaveChanges();
             
         }
@@ -117,9 +100,10 @@ namespace slnLogica
 
         public void actualizaUsuario(int Id, string correo, string contrasena,int rol)
         {
+            string clave = this.encriptarClaveUsuario(contrasena);
             Usuario usu = this.obtenUsuarioSegunIdentificador(Id);
             usu.Email = correo;
-            usu.Clave = contrasena;
+            usu.Clave = clave;
             usu.IdRol = rol;
             this.contexto.SaveChanges();
         }
