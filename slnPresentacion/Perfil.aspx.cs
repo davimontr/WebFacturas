8db309865cc4a2c2bbe242fd223c6fd724a1d49c
Kenneth Bolivar Castro <keboca@gmail.com>
2017-10-15 20:08:03 -0600

    Perfil form actualizado

--- slnPresentacion/Perfil.aspx.cs
+++ slnPresentacion/Perfil.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using slnLogica;
+using slnDatos;
 
 namespace slnPresentacion
 {
@@ -8,7 +9,7 @@ namespace slnPresentacion
         private IServiciosRoles roles = new AccionesRoles();
         private IServiciosUsuarios usuarios = new AccionesUsuarios();
 
-        protected void Page_Load(object sender, EventArgs e)
+        private void cargarRoles()
         {
             try
             {
@@ -21,9 +22,34 @@ namespace slnPresentacion
             }
         }
 
+        private void cargarUsuarioSesion()
+        {
+            Usuario sesion = (Usuario)Page.Session["sesion"];
+            this.txtEmail.Text = sesion.Email;
+            this.txtClave.Attributes["value"] = sesion.Clave;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                this.cargarRoles();
+                this.cargarUsuarioSesion();
+            }
+        }
+
         protected void btnSalvar_Click(object sender, EventArgs e)
         {
-
+            try {
+                Usuario sesion = (Usuario)Page.Session["sesion"];
+                this.usuarios.actualizaUsuario(sesion.Id, this.txtEmail.Text, this.txtClave.Text, int.Parse(this.ddlRoles.SelectedValue));
+                new SesionMensajes(Page).crearAviso("Perfil salvado.");
+                Response.Redirect("~/Dasboard.aspx");
+            }
+            catch (Exception ex)
+            {
+                this.lblMensaje.Text = ex.Message;
+            }
         }
     }
 }
