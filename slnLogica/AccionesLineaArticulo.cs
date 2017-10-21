4bfc836eabd4740e59c80fc20ee5e75ce7418c6f
Kenneth Bolivar Castro <keboca@gmail.com>
2017-10-20 22:44:39 -0600

    Actualizado lineaArticulos

--- slnLogica/AccionesLineaArticulo.cs
+++ slnLogica/AccionesLineaArticulo.cs
@@ -11,9 +11,10 @@ namespace slnLogica
     {
         List<LineaArticulo> obtenerTodos();
         void incluirLineaArticulo(Producto Producto, int Cantidad, int IdFactura);
-        void actualizaLineaArticulos(int Id, int IdProducto, int Cantidad, int IdFactura);
+        void actualizaLineaArticulos(int Id, Producto Producto, int Cantidad, int IdFactura);
         void eliminarLineaArticulo(int Id);
         List<LineaArticulo> obtenerTodosPorIdFactura(int IdFactura);
+        LineaArticulo obtenerSegunProductoFactura(int IdProducto, int IdFactura);
     }
 
     public   class AccionesLineaArticulo : AccionesEntidades, IserviciosLineaArticulo
@@ -24,33 +25,57 @@ namespace slnLogica
             return this.contexto.LineaArticuloes.ToList();
         }
 
-        // metodo de agregar
-        public void incluirLineaArticulo(Producto Producto, int Cantidad, int IdFactura)
+        protected decimal calcularPrecio(Producto Producto, int Cantidad)
         {
             decimal impuesto = 0;
             if (Producto.Gravado)
             {
-                impuesto = Producto.Costo * (Producto.Impuesto / 100);
+                impuesto = Producto.Costo * (decimal.Parse(Producto.Impuesto.ToString()) / 100);
             }
-            decimal utilidad = Producto.Costo * (Producto.Utilidad / 100);
 
-            this.contexto.LineaArticuloes.Add(new LineaArticulo
+            decimal utilidad = Producto.Costo * (decimal.Parse(Producto.Utilidad.ToString()) / 100);
+
+            return (Producto.Costo + impuesto + utilidad) * Cantidad;
+        }
+
+        public LineaArticulo obtenerSegunProductoFactura(int IdProducto, int IdFactura)
+        {
+            return this.contexto.LineaArticuloes
+                .Where(ln => ln.IdProducto == IdProducto && ln.IdFactura == IdFactura)
+                .FirstOrDefault()
+        }
+
+        // metodo de agregar
+        public void incluirLineaArticulo(Producto Producto, int Cantidad, int IdFactura)
+        {
+
+            LineaArticulo linea = this.obtenerSegunProductoFactura(Producto.Id, IdFactura);
+            if (linea == null)
             {
-                IdProducto = Producto.Id,
-                Cantidad = Cantidad,
-                IdFactura = IdFactura,
-                Precio = (Producto.Costo + impuesto + utilidad) * Cantidad
-            });
-            this.contexto.SaveChanges();
+
+                this.contexto.LineaArticuloes.Add(new LineaArticulo
+                {
+                    IdProducto = Producto.Id,
+                    Cantidad = Cantidad,
+                    IdFactura = IdFactura,
+                    Precio = this.calcularPrecio(Producto, Cantidad)
+                });
+                this.contexto.SaveChanges();
+            }
+            else
+            {
+                this.actualizaLineaArticulos(linea.Id, Producto, Cantidad, IdFactura);
+            }
         }
 
         //actualiza articulos 
-        public void actualizaLineaArticulos(int Id,int IdProducto, int Cantidad, int IdFactura)
+        public void actualizaLineaArticulos(int Id, Producto Producto, int Cantidad, int IdFactura)
         {
             LineaArticulo linea = this.obtenLineaArticuloSegunIdentificador(Id);
-            linea.IdProducto = IdProducto;
+            linea.IdProducto = Producto.Id;
             linea.Cantidad = Cantidad;
             linea.IdFactura = IdFactura;
+            linea.Precio = this.calcularPrecio(Producto, Cantidad);
             this.contexto.SaveChanges();
         }
         
@@ -59,7 +84,6 @@ namespace slnLogica
         {
             LineaArticulo linea = this.obtenLineaArticuloSegunIdentificador(Id);
             this.contexto.LineaArticuloes.Remove(linea);
-
         }
 
         public LineaArticulo obtenLineaArticuloSegunIdentificador(int Id)
