430b99d058587e474ce6d4834a9e71f0cc57e855
Kenneth Bolivar Castro <keboca@gmail.com>
2017-11-01 22:55:02 -0600

    Export to PDF feature

--- slnPresentacion/Dashboard.aspx.designer.cs
+++ slnPresentacion/Dashboard.aspx.designer.cs
@@ -38,5 +38,14 @@ namespace WebFacturas {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblMensaje;
+        
+        /// <summary>
+        /// btnExportar control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnExportar;
     }
 }
